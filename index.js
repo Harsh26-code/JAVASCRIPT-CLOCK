82f214b9fe02620fbba2f65c646ead02197272fb
Harsh26-code <83331122+Harsh26-code@users.noreply.github.com>
2022-11-21 14:04:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+// JavaScript Variables
+// Variables are the containers to store data values.
+  var a=45;
+  var b=78;
+//   console.log(a+b); 
+  var str="I am learning JavaScript";
+  var marks={
+    Harsh:98,
+    Amit:99,
+    Arnav:96,
+    Harry:87
+  }
+  var num2=87.9;
+   var c;
+//    console.log(arr);
+   var a=[1,2,3,4,"harsh",true];
+   var arr=[1,2,3,4,5.6,7,8.9];
+//    console.log(arr[0]);
+//    console.log(arr[1]);
+   let b1=10;
+   function solve(a)
+   {
+    var b1=5;
+    return a+b;
+   }
+  //  console.log(b1);
+  let date=new Date();
+  // console.log(date.getDay());
+   //DOM Manipulation
+   let cd=document.getElementById('click');
+   console.log(cd);
+  
+
+
+   
